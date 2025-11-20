93beb3cb0997af80ba5caa0c9fabfa4d1b588643
kaushikazala <kaushikazala4@gmail.com>
2025-11-20 16:25:52 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import "./styles/theme.css";
 import "./App.css";
 import AppRoutes from "./routes/AppRoutes";
 
-export const API_URL = "https://reel-backend-nckx.onrender.com/";
+export const API_URL = "https://reel-backend-nckx.onrender.com";
 
 function App() {
   return (
